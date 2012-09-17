a69f8fbafe31bc58c581b6d231fd9b894d3264bd
Kevin Moormann <kevin.moormann@parivedasolutions.com>
2012-09-16 23:48:22 -0500

    initial commit

---
+++ Controllers/KMLController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.KML.Controllers
+{
+    public class KMLController : Controller
+    {
+        //
+        // GET: /KML/
+
+        public ActionResult Index()
+        {
+            var officePlaceMarks = new List<Models.KMPlaceMarkLViewModel>()
+            {
+                new Models.KMPlaceMarkLViewModel(){ Name ="Dallas", Description = "2811 McKinney Ave", Point="-96.8039,32.7828"},
+                new Models.KMPlaceMarkLViewModel(){ Name ="Houston", Description = "4203 Montrose Blvd", Point="-95.3631,29.7631"}
+            };
+            return View(officePlaceMarks);
+        }
+
+    }
+}
---
+++ Models/KMLViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC.KML.Models
+{
+    public class KMPlaceMarkLViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Point { get; set; }
+
+    }
+}
